8ad93fb684bf0324ded5a9d85645a617308e440f
PCHinkel <pchinkel@crimson.ua.edu>
2020-07-12 19:46:20 -0400

    Update table-app.js

--- static/ph/table-app.js
+++ static/ph/table-app.js
@@ -1,25 +1,6 @@
-// Select the button
-var button = d3.select("#button");
-var reset = d3.select("#reset-button");
-// Select the form
-var form = d3.select("#form");
-// Create event handlers 
-button.on("click", runEnter);
-form.on("submit",runEnter);
-reset.on("click", runReset);
-// Get a reference to the table body
 var tbody = d3.select("tbody");
 
-url = "/raw-data"
-d3.json(url).then(function(response) {
-  console.log(response);
-}).catch(function(error) 
-{return console.log(error);}
-);
-
-
 console.log(data);
-
 data.forEach((meteorite) => {
   var row = tbody.append("tr");
   Object.entries(meteorite).forEach(([key, value]) => {
@@ -27,52 +8,6 @@ data.forEach((meteorite) => {
     cell.text(value);
   });
 });
-
-function runReset() {
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
-  var tbody = d3.select("tbody");
-  tbody.html("");
-  data.forEach((meteorite) => {
-    var row = tbody.append("tr");
-    Object.entries(meteorite).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
-    });
-  });
-};
-
-// Complete the event handler function for the form
-function runEnter() {
-
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
-  
-  // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#year");
-
-  // Get the value property of the input element
-  var inputValue = inputElement.property("value");
-
-  console.log(inputValue);
-
-  var filteredData = data.filter(data => parseInt(data.year) === parseInt(inputValue));
-
-  var tbody = d3.select("tbody");
-
-  tbody.html("");
-
-  console.log(filteredData);
-
-  filteredData.forEach((meteorite) => {
-    var row = tbody.append("tr");
-    Object.entries(meteorite).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
-    });
-  });
-
-};
 
 $(document).ready( function () {
   $('#meteorite-table').DataTable();
